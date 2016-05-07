eb2801db1bc4cbed1cfcc601c53f8f8b4762cc79
augustar <augustar@c76f9247-7709-4348-9403-dde0860217d6>
2016-05-07 19:06:14 +0000

    Ant redirected to create JAR files to target directory. It was made for unification of builds with Maven.

--- testing-lib/tools_win/README!!!.txt
+++ testing-lib/tools_win/README!!!.txt
@@ -2,9 +2,11 @@ Tato složka obsahuje nástroje používané ve Windows 7 professional.
 Aby bylo možné použít nástroj Java API compliance checker (JAPICC) je nutné mít nainstalovaný perl.
 
 Složka "reports" obsahuje vygenerované reporty jednotlivých nástrojù
-složka "test_lib" obsahuje testovací knihovny 
+
+Složka "test_lib" obsahuje testovací knihovny 
 pro nástroje jour a sigtest je zapotøebí nahrát do složky "test_lib" i knihovnu "rt.jar", která se defaultnì nachází v instalaci Javy ve složce JRE/lib.
 Popøípadì je nutné upravit spouštìcí soubory, aby v classpath byla skuteèná cesta k této knihovnì.
+!!! nastroj jour vyhazuje u nìkterých testù vyjímku. Proto má vytvoøené knihovny neobsahující tyto zmìny ve své složce (složitìjší pøidávání testù) !!!
 
 soubor: "runTools.bat" - spustí postupnì všechny nástroje a vygeneruje reporty do pøipravené složky "reports"
 
--- testing-lib/tools_win/runTools.bat
+++ testing-lib/tools_win/runTools.bat
@@ -1,20 +1,20 @@
 echo ********* Clirr *********
-java -jar clirr/clirr-core-0.6-uber.jar -o test_lib/testing-lib-v1-0.0.1.jar -n test_lib/testing-lib-v2-0.0.2.jar -f reports/clirrReport.txt
+java -jar clirr/clirr-core-0.6-uber.jar -o ../testing-lib-v1/target/testing-lib-v1-0.0.1.jar -n ../testing-lib-v2/target/testing-lib-v2-0.0.2.jar -f reports/clirrReport.txt
 echo ********* JaCC *********
-java -jar jacc/jacc.jar test_lib/testing-lib-v1-0.0.1.jar test_lib/testing-lib-v2-0.0.2.jar reports/jaccReport.txt
+java -jar jacc/jacc.jar ../testing-lib-v1/target/testing-lib-v1-0.0.1.jar ../testing-lib-v2/target/testing-lib-v2-0.0.2.jar reports/jaccReport.txt
 echo ********* japi checker *********
-java -jar japi_checker/japi-checker-cli-0.2.1-SNAPSHOT.jar test_lib/testing-lib-v1-0.0.1.jar test_lib/testing-lib-v2-0.0.2.jar -bin > reports/japiCheckerReport.txt
+java -jar japi_checker/japi-checker-cli-0.2.1-SNAPSHOT.jar ../testing-lib-v1/target/testing-lib-v1-0.0.1.jar ../testing-lib-v2/target/testing-lib-v2-0.0.2.jar -bin > reports/japiCheckerReport.txt
 echo ********* japicmp *********
-java -jar japicmp/japicmp-0.7.2-jar-with-dependencies.jar -o test_lib/testing-lib-v1-0.0.1.jar -n test_lib/testing-lib-v2-0.0.2.jar -a private > reports/japicmpReport.txt
+java -jar japicmp/japicmp-0.7.2-jar-with-dependencies.jar -o ../testing-lib-v1/target/testing-lib-v1-0.0.1.jar -n ../testing-lib-v2/target/testing-lib-v2-0.0.2.jar -a private > reports/japicmpReport.txt
 echo ********* sigtest *********
-java -jar sigtest/sigtestdev.jar SetupAndTest -reference test_lib/testing-lib-v1-0.0.1.jar;test_lib/rt.jar -test test_lib/testing-lib-v2-0.0.2.jar;test_lib/rt.jar -package testing_lib -H -out reports/sigtestReport.txt
+java -jar sigtest/sigtestdev.jar SetupAndTest -reference ../testing-lib-v1/target/testing-lib-v1-0.0.1.jar;lib_dependencies/rt.jar -test ../testing-lib-v2/target/testing-lib-v2-0.0.2.jar;lib_dependencies/rt.jar -package testing_lib -H -out reports/sigtestReport.txt
 
 echo ********* Jour *********
 echo vytvoreni signature souboru Jour:
-java -cp jour/jour-instrument-2.0.3.jar;jour/javassist.jar net.sf.jour.SignatureGenerator --src jour/testing-lib-v1-0.0.1.jar -jars test_lib/rt.jar --packages testing_lib --dst jour/sigTestLib1ApiSignature.xml --level private
+java -cp jour/jour-instrument-2.0.3.jar;jour/javassist.jar net.sf.jour.SignatureGenerator --src jour/testing-lib-v1-0.0.1.jar -jars lib_dependencies/rt.jar --packages testing_lib --dst jour/sigTestLib1ApiSignature.xml --level private
 
 echo Spusteni testu Jour:
-java -cp jour/jour-instrument-2.0.3.jar;jour/javassist.jar net.sf.jour.SignatureVerify --src jour/testing-lib-v2-0.0.2.jar -jars test_lib/rt.jar --signature jour/sigTestLib1ApiSignature.xml --level private > reports/jourReport.txt
+java -cp jour/jour-instrument-2.0.3.jar;jour/javassist.jar net.sf.jour.SignatureVerify --src jour/testing-lib-v2-0.0.2.jar -jars lib_dependencies/rt.jar --signature jour/sigTestLib1ApiSignature.xml --level private > reports/jourReport.txt
 
 echo ********* japicc *********
-perl japi-compliance-checker-1.5/japi-compliance-checker test_lib/testing-lib-v1-0.0.1.jar test_lib/testing-lib-v2-0.0.2.jar -report-path reports/japiccReport.html
+perl japi-compliance-checker-1.5/japi-compliance-checker ../testing-lib-v1/target/testing-lib-v1-0.0.1.jar ../testing-lib-v2/target/testing-lib-v2-0.0.2.jar -report-path reports/japiccReport.html
