bb35659c495c83a596ce204d821008dea3788962
Kamil Jezek <kamil.jezek@gmail.com>
2016-07-14 15:22:25 +0200

    clean-up

--- tools/run.sh
+++ tools/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+RT_JAR="${JAVA_HOME}/jre/lib/rt.jar"
+
 echo "********* Japitools *********"
 japize as japitools/japizeSigFile packages ../lib-v1.jar lib_dependencies/rt.jar +testing_lib
 japize as japitools/japizeSigFile2 packages ../lib-v2.jar lib_dependencies/rt.jar +testing_lib
@@ -24,8 +26,8 @@ echo "********* sigtest *********"
 java -jar sigtest/sigtestdev.jar SetupAndTest -reference ../lib-v1.jar:lib_dependencies/rt.jar -test ../lib-v2.jar:lib_dependencies/rt.jar -package testing_lib -H -out .reports/sigtest.txt
 
 echo "********* Jour *********"
-java -cp jour/jour-instrument-2.0.3.jar:jour/javassist.jar net.sf.jour.SignatureGenerator --src jour/lib-v1.jar -jars lib_dependencies/rt.jar --packages testing_lib --dst jour/sigTestLib1ApiSignature.xml --level private
-java -cp jour/jour-instrument-2.0.3.jar:jour/javassist.jar net.sf.jour.SignatureVerify --src jour/lib-v2.jar -jars lib_dependencies/rt.jar --signature jour/sigTestLib1ApiSignature.xml --level private > .reports/jour.txt
+java -cp jour/jour-instrument-2.0.3.jar:jour/javassist.jar net.sf.jour.SignatureGenerator --src ../lib-v1.jar -jars lib_dependencies/rt.jar --packages testing_lib --dst jour/sigTestLib1ApiSignature.xml --level private
+java -cp jour/jour-instrument-2.0.3.jar:jour/javassist.jar net.sf.jour.SignatureVerify --src ../lib-v2.jar -jars lib_dependencies/rt.jar --signature jour/sigTestLib1ApiSignature.xml --level private > .reports/jour.txt
 
 echo "********* japicc *********"
 perl japi-compliance-checker-1.5/japi-compliance-checker ../lib-v1.jar ../lib-v2.jar -report-path .reports/japicc.html
