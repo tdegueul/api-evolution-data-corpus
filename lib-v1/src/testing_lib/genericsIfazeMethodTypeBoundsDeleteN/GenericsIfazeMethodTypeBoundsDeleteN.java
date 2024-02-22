23c72974b280778b7c3600147e7a5a548d88ad6a
Thomas Degueule <thomas.degueule@labri.fr>
2024-02-22 17:14:56 +0100

    No common prefix in case names to avoid grep's misinterpretation

--- lib-v1/src/testing_lib/genericsIfazeMethodTypeBoundsDeleteN/GenericsIfazeMethodTypeBoundsDeleteN.java
+++ lib-v1/src/testing_lib/genericsIfazeMethodTypeBoundsDeleteN/GenericsIfazeMethodTypeBoundsDeleteN.java
@@ -1,6 +1,6 @@
 package testing_lib.genericsIfazeMethodTypeBoundsDeleteN;
 
-public interface GenericsIfazeMethodTypeBoundsDelete {
+public interface GenericsIfazeMethodTypeBoundsDeleteN {
 	
 	public <T extends Number> void method1();
 	
