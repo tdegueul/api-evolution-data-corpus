4c3b6d0f453ad83beac00de3d088fdacee624720
Corentin Latappy <corentin.latappy@labri.fr>
2024-10-28 09:44:07 +0100

    🐛 Fixing v2 of AccessModifierIfazeFieldAccessDecreasePublicToNon where v1 and v2 were similar

--- lib-v2/src/testing_lib/accessModifierIfazeFieldAccessDecreasePublicToNon/AccessModifierIfazeFieldAccessDecreasePublicToNon.java
+++ lib-v2/src/testing_lib/accessModifierIfazeFieldAccessDecreasePublicToNon/AccessModifierIfazeFieldAccessDecreasePublicToNon.java
@@ -2,6 +2,6 @@ package testing_lib.accessModifierIfazeFieldAccessDecreasePublicToNon;
 
 public interface AccessModifierIfazeFieldAccessDecreasePublicToNon {
 
-	public Integer FIELD_PUBLIC_TO_NON = 5;
+	Integer FIELD_PUBLIC_TO_NON = 5;
 	
 }
