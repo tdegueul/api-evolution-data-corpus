7ce2dc65e81e92f1300c0d7f9f29022dc5e75a9c
Thomas Degueule <thomas.degueule@labri.fr>
2025-01-29 19:03:24 +0100

    Bump Roseau & japicmp

--- tools/run.sh
+++ tools/run.sh
@@ -27,7 +27,7 @@ echo "********* Revapi *********"
 record_execution_time "Revapi" "tools/revapi/revapi.sh --extensions=org.revapi:revapi-java:0.28.1,org.revapi:revapi-reporter-text:0.15.0 --old=output/build/lib-v1.jar --new=output/build/lib-v2.jar -Drevapi.reporter.text.minSeverity=POTENTIALLY_BREAKING > $REPORTS_DIRECTORY/revapi.txt"
 
 echo "********* japicmp *********"
-record_execution_time "japicmp" "java -jar tools/japicmp/japicmp-0.23.0-jar-with-dependencies.jar -o output/build/lib-v1.jar -n output/build/lib-v2.jar -b -m > $REPORTS_DIRECTORY/japicmp.txt"
+record_execution_time "japicmp" "java -jar tools/japicmp/japicmp-0.23.1-jar-with-dependencies.jar -o output/build/lib-v1.jar -n output/build/lib-v2.jar -b -m > $REPORTS_DIRECTORY/japicmp.txt"
 
 echo "********* Roseau *********"
 record_execution_time "Roseau" "java -jar tools/roseau/roseau-0.0.4-SNAPSHOT-jar-with-dependencies.jar --diff --v1 lib-v1 --v2 lib-v2  > $REPORTS_DIRECTORY/roseau.txt"
